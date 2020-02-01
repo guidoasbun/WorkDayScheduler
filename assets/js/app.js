1e3ad8422a9713334afee934bb4936ace572ac5d
Guido <Asbun>
2020-02-01 09:51:23 -0800

    array changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,18 @@
 let currentDay = moment().format("dddd, MMMM Do YYYY")
-let currentHour = moment().hour()
-const hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+let currentHour = '1 PM'//moment().format("h A")
+console.log(typeof(currentHour))
+const hours = [
+  '9 AM', 
+  '10 AM', 
+  '11 AM', 
+  '12 PM', 
+  '1 PM', 
+  '2 PM', 
+  '3 PM', 
+  '4 PM', 
+  '5 PM'
+]
+console.log(typeof(hours[1]))
 
 $('#currentDay').html(currentDay)
 
@@ -9,6 +21,7 @@ const scheduleTable = $('#scheduleTable')
 
 function createSchedule() {
   for (let i = 0; i < hours.length; i++) {
+  
   let timeElem = $(`
   <div class="container">
   <div>
@@ -18,16 +31,19 @@ function createSchedule() {
           <p>${hours[i]}</p>
         </div>
         <div class="col present">
-          <input type="text" class="form-control-plaintext present" placeholder="State">
+          <input type="text" name="text" id="text" class="form-control-plaintext present" placeholder=" ">
         </div>
         <div class="col-1 saveBtn ">
-          <button type="submit" class="btn mb-2"><i class="far fa-save"></i></button>
+          <button id="submit" type="submit" class="btn mb-2"><i class="far fa-save"></i></button>
         </div>
       </div>
     </form>
   </div>
   </div>
-`)
+  `)
+
+  
+
 $('#scheduleTable').append(timeElem)
 }
 }
