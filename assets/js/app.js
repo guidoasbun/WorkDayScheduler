6242b76f62b08347c82cc309a70c9c8eb53140e2
Guido <Asbun>
2020-01-31 21:07:18 -0800

    created HTML template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,33 @@
-let now = moment()
+let currentDay = moment().format("dddd, MMMM Do YYYY")
+let currentHour = moment().hour()
+const hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
 
-// console.log(now.format("dddd, MMMM Do YYYY"))
+$('#currentDay').html(currentDay)
 
-$('#currentDay').html(now.format("dddd, MMMM Do YYYY"))
 
+const scheduleTable = $('#scheduleTable')
+
+function createSchedule() {
+  for (let i = 0; i < hours.length; i++) {
+  scheduleTable.html(`
+  <div class="container">
+  <div>
+    <form>
+      <div class="form-row align-items-center row">
+        <div class="col-2 bg-light hour">
+          <p>${hours[i]}</p>
+        </div>
+        <div class="col present">
+          <input type="text" class="form-control-plaintext present" placeholder="State">
+        </div>
+        <div class="col-1 saveBtn ">
+          <button type="submit" class="btn mb-2"><i class="far fa-save"></i></button>
+        </div>
+      </div>
+    </form>
+  </div>
+  </div>
+`)
+}
+}
+createSchedule()
